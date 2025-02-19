6f4cda1c4026b418b375d0c924511e00ebb47048
pooxhi <rak.mariano6262@gmail.com>
2025-02-19 12:07:42 +0800

    Update reactive-form.component.ts

--- src/app/reactive-form/reactive-form.component.ts
+++ src/app/reactive-form/reactive-form.component.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-reactive-form',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule, RouterModule],
   templateUrl: './reactive-form.component.html',
   styleUrl: './reactive-form.component.css'
 })
 export class ReactiveFormComponent {
+  title = 'form-app';
+  firstName: string = "";
+  lastName: string = "";
+  email: string = "";
+  password: string = "";
 
+  formdata: FormGroup = new FormGroup({
+    firstName: new FormControl(""),
+    lastName: new FormControl(""),
+    email: new FormControl(""),
+    password: new FormControl("")
+  });
+
+  onClickSubmit(data: { firstName: string; lastName: string; email: string; password: string }) {
+    this.firstName = data.firstName;
+    this.lastName = data.lastName;
+    this.email = data.email;
+    this.password = data.password;
+  }
 }
