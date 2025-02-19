90f2bb80c5ccf094b4aeee54df7b1042a9609d0e
pooxhi <rak.mariano6262@gmail.com>
2025-02-19 12:07:54 +0800

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { TemplateDrivenComponent } from './template-driven/template-driven.component';
+import { ReactiveFormComponent } from './reactive-form/reactive-form.component';
 
 export const routes: Routes = [
     {path : 'template', component : TemplateDrivenComponent},
+    {path : 'reactive', component : ReactiveFormComponent},
     
 ];
